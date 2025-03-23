4bba5f463ef520f93b7653e8cd7abdcf9d4b354a
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-03-23 22:09:38 +0600

    extracting displayMeasurementInstructions() into 3 parts

--- FlightDistance.java
+++ FlightDistance.java
@@ -3,15 +3,22 @@ public abstract class FlightDistance {
 
     public abstract String[] calculateDistance(double lat1, double lon1, double lat2, double lon2);
 
-    public void displayMeasurementInstructions(){
+    public void displayMeasurementInstructions() {
         String symbols = "+---------------------------+";
+        printHeader(symbols);
+        printGuidelines();
+    }
+
+    private void printHeader(String symbols) {
         System.out.printf("\n\n %100s\n %100s", symbols, "| SOME IMPORTANT GUIDELINES |");
         System.out.printf("\n %100s\n", symbols);
-        System.out.println("\n\t\t1. Distance between the destinations are based upon the Airports Coordinates(Latitudes && Longitudes) based in those cities\n");
-        System.out.println("\t\t2. Actual Distance of the flight may vary from this approximation as Airlines may define their on Travel Policy that may restrict the planes to fly through specific regions...\n");
-        System.out.println("\t\t3. Flight Time depends upon several factors such as Ground Speed(GS), AirCraft Design, Flight Altitude and Weather. Ground Speed for these calculations is 450 Knots...\n");
+    }
+
+    private void printGuidelines() {
+        System.out.println("\n\t\t1. Distance between the destinations are based upon the Airports Coordinates (Latitudes & Longitudes) based in those cities\n");
+        System.out.println("\t\t2. Actual Distance of the flight may vary from this approximation as Airlines may define their own Travel Policy that may restrict the planes to fly through specific regions...\n");
+        System.out.println("\t\t3. Flight Time depends upon several factors such as Ground Speed (GS), Aircraft Design, Flight Altitude, and Weather. Ground Speed for these calculations is 450 Knots...\n");
         System.out.println("\t\t4. Expect reaching your destination early or late from the Arrival Time. So, please keep a margin of ±1 hour...\n");
         System.out.println("\t\t5. The departure time is the moment that your plane pushes back from the gate, not the time it takes off. The arrival time is the moment that your plane pulls into the gate, not the time\n\t\t   it touches down on the runway...\n");
     }
-
 }
