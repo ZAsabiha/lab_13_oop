76a47b78f9dd57ed9a3b52ba47cde1bde0300676
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-03-23 21:57:25 +0600

    extract class customer to cusyomerManager

--- CutomerManager.java
+++ CutomerManager.java
@@ -2,7 +2,9 @@
 import java.util.*;
 
 public class CustomerManager {
-    private static final List<Customer> customerCollection = new ArrayList<>();
+    public List<Flight> flightsRegisteredByUser;
+    public List<Integer> numOfTicketsBookedByUser;
+    public static final List<Customer> customerCollection = User.getCustomersCollection();
 
     public void addNewCustomer(String name, String email, String password, String phone, String address, int age) {
         RandomGenerator random = new RandomGenerator();
@@ -26,10 +28,64 @@ public class CustomerManager {
         System.out.println("Customer with ID " + ID + " deleted.");
     }
 
-    public void displayCustomersData() {
-        CustomerUtils.displayHeader();
+
+      public boolean isUniqueData(String emailID) {
+        boolean isUnique = false;
         for (Customer c : customerCollection) {
-            System.out.println(CustomerUtils.formatCustomerData(c));
+            if (emailID.equals(c.getEmail())) {
+                isUnique = true;
+                break;
+            }
+        }
+        return isUnique;
+    }
+      public void displayCustomersData(boolean showHeader) {
+        displayHeader();
+        Iterator<Customer> iterator = customerCollection.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            i++;
+            Customer c = iterator.next();
+            System.out.println(c.toString(i));
+            System.out.printf(
+                    "%10s+------------+------------+----------------------------------+---------+-----------------------------+-------------------------------------+-------------------------+\n",
+                    "");
         }
     }
+
+    void displayHeader() {
+        System.out.println();
+        System.out.printf(
+                "%10s+------------+------------+----------------------------------+---------+-----------------------------+-------------------------------------+-------------------------+\n",
+                "");
+        System.out.printf(
+                "%10s| SerialNum  |   UserID   | Passenger Names                  | Age     | EmailID\t\t       | Home Address\t\t\t     | Phone Number\t       |%n",
+                "");
+        System.out.printf(
+                "%10s+------------+------------+----------------------------------+---------+-----------------------------+-------------------------------------+-------------------------+\n",
+                "");
+        System.out.println();
+
+    }
+        String randomIDDisplay(String randomID) {
+        StringBuilder newString = new StringBuilder();
+        for (int i = 0; i <= randomID.length(); i++) {
+            if (i == 3) {
+                newString.append(" ").append(randomID.charAt(i));
+            } else if (i < randomID.length()) {
+                newString.append(randomID.charAt(i));
+            }
+        }
+        return newString.toString();
+    }
+    void addNewFlightToCustomerList(Flight f) {
+        this.flightsRegisteredByUser.add(f);
+     
+    }
+
+    void addExistingFlightToCustomerList(int index, int numOfTickets) {
+        int newNumOfTickets = numOfTicketsBookedByUser.get(index) + numOfTickets;
+        this.numOfTicketsBookedByUser.set(index, newNumOfTickets);
+    }
+
 }
